a9ec3c6d46337f9032e3dba2661a6399938e5294
PROF ROMULO <rjlpena@gmail.com>
2025-09-02 13:35:15 +0000

    ok

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+
 botaoMostraPalavras.addEventListener('click',mostraPalavrasChave);
+
 function mostraPalavrasChave(){
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
     const palavrasChave = processaTexto(texto);
+    
     campoResultado.textContent = palavrasChave.join(", ");
 }
 
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
+    
     const frequencias = contaFrequencias(palavras);
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
+    
     function ordenaPalavra(p1,p2){
         return frequencias[p2] - frequencias[p1];
     }
+    
     console.log(ordenadas);
     return ordenadas.slice(0,10);
 }
@@ -29,6 +35,6 @@ function contaFrequencias(palavras){
       }
     }
 }
-    //console.log(frequencias);
+    
     return frequencias;
 }
