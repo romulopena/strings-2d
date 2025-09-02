f1ca958ce67970fc2a7117b9175650faf283c747
PROF ROMULO <rjlpena@gmail.com>
2025-09-02 14:59:52 +0000

    aula 8

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import {PALAVRAS_RUINS} from "./palavrasRuins.js"
+
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener('click',mostraPalavrasChave);
@@ -12,7 +14,10 @@ function mostraPalavrasChave(){
 
 function processaTexto(texto){
     let palavras = texto.split(/\P{L}+/u);
-    
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();//LETRAS MINUSCULAS
+    }
+    palavras = tiraPalavrasRuins(palavras);
     const frequencias = contaFrequencias(palavras);
     let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
     
@@ -37,4 +42,18 @@ function contaFrequencias(palavras){
 }
     
     return frequencias;
+}
+
+function tiraPalavrasRuins(palavras) {
+  const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
+
+  const palavrasBoas = [];
+
+  for (let palavra of palavras) {
+    if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+      palavrasBoas.push(palavra);
+    }
+  }
+
+  return palavrasBoas;
 }
